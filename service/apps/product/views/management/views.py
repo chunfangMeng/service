5f498a8017a03b0f7347e94eb78bec5a60024792
mengchunfang <mengchunfang@quseit.com>
2023-05-15 19:44:17 +0800

    refactor: format

--- service/apps/product/views/management/views.py
+++ service/apps/product/views/management/views.py
@@ -104,8 +104,6 @@ class ProductBrandView(GenericViewSet, ListModelMixin, CreateModelMixin, UpdateM
         self.perform_update(serializer)
 
         if getattr(instance, '_prefetched_objects_cache', None):
-            # If 'prefetch_related' has been applied to a queryset, we need to
-            # forcibly invalidate the prefetch cache on the instance.
             instance._prefetched_objects_cache = {}
 
         return JsonResponse(serializer.data)
