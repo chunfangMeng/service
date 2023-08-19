674d0a13fd76b2d89c68735fc7869745498435cf
mengchunfang <mengchunfang@quseit.com>
2023-08-20 04:17:25 +0800

    refactor: permission

--- service/apps/manager/views/staff/manager_views.py
+++ service/apps/manager/views/staff/manager_views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import User, Permission
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import GenericViewSet
 
@@ -6,14 +8,49 @@ from apps.manager.models import ManagerUser
 from apps.manager.views.staff.filters import StaffFilter
 from apps.manager.views.staff.serializers import ManagerStaffSerializer
 from drf.auth import ManageAuthenticate
-from drf.mixins import ListModelMixin
-from drf.permission import ManagerPermission
+from drf.mixins import ListModelMixin, RetrieveModelMixin
+from drf.permission import ManagerPermission, CUSTOM_PERMISSIONS, PERMISSION_GROUP
+from drf.response import JsonResponse
 
 
-class ManagerStaffView(GenericViewSet, ListModelMixin):
+class ManagerStaffView(GenericViewSet, ListModelMixin, RetrieveModelMixin):
     authentication_classes = [ManageAuthenticate, ]
     permission_classes = [ManagerPermission, ]
     queryset = ManagerUser.objects.all().order_by('-id')
     serializer_class = ManagerStaffSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_class = StaffFilter
+    permission_list = ['staff_view', ]
+
+    @action(methods=['get'], detail=True, permission_list=['staff_change'], url_path='user/permission')
+    def get_user_permission(self, request, pk):
+        all_permission = []
+        try:
+            user_id = int(pk)
+            print(user_id)
+        except:
+            return JsonResponse(code=4043, message='用户ID错误')
+        has_permission = []
+        manager_user = ManagerUser.objects.filter(
+            id=user_id
+        ).first()
+        for permission in list(manager_user.user.get_user_permissions()):
+            has_permission.append(permission.split('.')[-1])
+        for group in PERMISSION_GROUP:
+            permission_prefix = group[0].split('_group')[0]
+            group_obj = {
+                'label': group[1],
+                'value': group[0],
+                'children': []
+            }
+            for group_item in CUSTOM_PERMISSIONS:
+                if group_item[0].find(permission_prefix) != -1:
+                    group_obj['children'].append({
+                        'label': group_item[1],
+                        'value': group_item[0]
+                    })
+            all_permission.append(group_obj)
+        return JsonResponse({
+            'all_permission': all_permission,
+            'has_permission': has_permission
+        })
--- service/drf/permission.py
+++ service/drf/permission.py
@@ -1,30 +1,33 @@
 from rest_framework.permissions import BasePermission
 
 
-CUSTOM_PERMISSIONS = (
+PERMISSION_GROUP = (
     ('staff_group', '员工管理'),
+    ('shop_brand_group', '商城品牌'),
+    ('shop_category_group', '商城分类'),
+    ('shop_attribute_group', '商城属性组'),
+    ('shop_product_group', '商城商品'),
+    ('member_group', '会员列表'),
+)
+CUSTOM_PERMISSIONS = (
     ('staff_view', '员工查看'),
     ('staff_change', '员工编辑'),
     ('staff_remove', '员工删除'),
     ('staff_add', '员工新增'),
-    ('shop_brand_group', '商城品牌'),
     ('shop_brand_add', '品牌新增'),
     ('shop_brand_change', '品牌修改'),
     ('shop_brand_delete', '品牌删除'),
     ('shop_brand_view', '品牌查看'),
     ('shop_brand_export', '品牌导出'),
-    ('shop_category_group', '商城分类'),
     ('shop_category_view', '商城分类查看'),
     ('shop_category_change', '商城分类修改'),
     ('shop_category_delete', '商城分类删除'),
     ('shop_category_add', '商城分类新增'),
     ('shop_category_export', '商城分类导出'),
-    ('shop_attribute_group', '商城属性组'),
     ('shop_attribute_view', '属性组查看'),
     ('shop_attribute_change', '属性组编辑'),
     ('shop_attribute_delete', '属性组删除'),
     ('shop_attribute_export', '属性组导出'),
-    ('shop_product_group', '商城商品'),
     ('shop_product_view', '商品查看'),
     ('shop_product_change', '商品编辑'),
     ('shop_product_delete', '商品删除'),
@@ -32,7 +35,6 @@ CUSTOM_PERMISSIONS = (
     ('shop_product_export', '商品导出'),
     ('shop_product_attribute_change', '商品属性设置'),
     ('shop_product_specs_change', '商品价格设置'),
-    ('member_group', '会员列表'),
     ('member_view', '会员查看'),
     ('member_all_view', '全会员查看'),
     ('member_change', '会员编辑'),
